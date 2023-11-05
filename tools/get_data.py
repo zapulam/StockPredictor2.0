f3c6864748d1322ec749e397cce65bf282ca1b89
Zack's PC <zapulliam22@gmail.com>
2023-11-05 13:01:51 -0600

    update get_data.py

--- tools/get_data.py
+++ tools/get_data.py
@@ -13,16 +13,7 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 
-def get_data(args):
-    '''
-    Downloads daily historical data for all S&P 500 stocks, should be ran with utils as cwd
-    
-    Inputs:
-        args (dict) - cmd line aruments for training
-            - info (str) - path to S&P500-Info.csv
-    '''
-    info, folder = args.info, 'daily_prices'
-
+def download_data(dir, info):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # this is chrome, you can set whatever browser you like
 
     now = dt.datetime.now()
@@ -35,11 +26,6 @@ def get_data(args):
     period2 = str(int((b-a).total_seconds()))   # total seconds from today since Jan. 1, 1970
 
     url = 'https://query1.finance.yahoo.com/v7/finance/download/{stock}?period1={period1}&period2={period2}&interval=1d&events=history&includeAdjustedClose=true'
-
-    dir = os.getcwd()
-    dir = dir.split(os.sep)
-    dir = os.path.join('C:\\', *dir[1:-1], folder)
-    os.makedirs(dir, exist_ok=True)
 
     # Get all ticker symbols from info file
     df = pd.read_csv(info)
@@ -56,6 +42,24 @@ def get_data(args):
             data.to_csv(os.path.join(dir, symbol + '.csv'), index = False)
         sys.stdout.write('\rGetting data for: %s - DONE' % symbol.ljust(5))
 
+
+def create_data_folder(args):
+    '''
+    Downloads daily historical data for all S&P 500 stocks, should be ran with utils as cwd
+    
+    Inputs:
+        args (dict) - cmd line aruments for training
+            - info (str) - path to S&P500-Info.csv
+    '''
+    info, folder = args.info, args.folder
+
+    dir = os.getcwd()
+    dir = dir.split(os.sep)
+    dir = os.path.join('C:\\', *dir[1:-1], folder)
+    os.makedirs(dir, exist_ok=True)
+
+    download_data(dir, info)
+
     sys.stdout.write('\rAll stock historical price files saved to daily_prices')
     
     
@@ -69,11 +73,12 @@ def parse_args():
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--info', type=str, default='S&P500-Info.csv', help='location of S&P500-Info.csv')
+    parser.add_argument('--folder', type=str, default='daily_prices', help='folder to save data')
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = parse_args()
-    get_data(args)
+    create_data_folder(args)
     
